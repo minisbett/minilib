819d77bf6515621c7f77b8cc0fe2659586f5a532
minisbett <39670899+minisbett@users.noreply.github.com>
2023-09-29 18:44:49 +0200

    Add string tests

---
+++ minilib/minilib.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using minilib.Extensions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minilib.Tests.Extensions;
+
+[TestFixture]
+internal class StringExtensionsTests
+{
+  [Test]
+  public void Successful_Format()
+  {
+    Assert.That("Foo {0}".Format("Bar"), Is.EqualTo("Foo Bar"));
+  }
+}
