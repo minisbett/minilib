a76f10a28675a27a634f85137913de52bb227df2
minisbett <39670899+minisbett@users.noreply.github.com>
2023-09-30 02:00:29 +0200

    Add string constants

---
+++ minilib/minilib.Tests/Constants/StringConstantsTests.cs
@@ -0,0 +1,21 @@
+﻿using minilib.Constants;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minilib.Tests.Constants;
+
+[TestFixture]
+internal class StringConstantsTests
+{
+  [Test]
+  public void Successful_PrintableAscii()
+  {
+    string should = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+    
+    Assert.That(StringConstants.PRINTABLE_ASCII, Is.EqualTo(should));
+  }
+}
---
+++ minilib/minilib/Constants/StringConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minilib.Constants;
+
+/// <summary>
+/// Provides common constant strings.
+/// </summary>
+public static class StringConstants
+{
+  /// <summary>
+  /// An array of all printable ASCII characters. (32-126)
+  /// </summary>
+  public static readonly char[] PRINTABLE_ASCII = Enumerable.Range(32, 126).Select(x => (char)x).ToArray();
+}
